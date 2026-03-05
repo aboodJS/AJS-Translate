49cc3b512a31ecb412f5102a0c01d62491c09eec
ajs <140010746+aboodJS@users.noreply.github.com>
2026-03-05 18:11:04 +0300

    styled the boxes added refs and v-models for their values the design is now more responsive

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import TranslateBox from './components/TranslateBox.vue';
+const translateLang = ref("en")
+const targetLang = ref("ar")
+
+const result: string = ref("")
+
+
+</script>
 
 <template>
- <main><h1>AJS Translate</h1></main>
+  <main class="h-full grid">
+    <h1 class="text-white font-bold text-center w-screen flex justify-center items-center">AJS Translate</h1>
+
+ <section class="md:flex h-fit  justify-center gap-x-4 max-md:grid max-md:content-center items-center">
+  <TranslateBox :editable="false" v-model="result"></TranslateBox>
+  <TranslateBox :editable="true" v-model="result"></TranslateBox>
+ </section>
+ </main>
 </template>
 
 <style scoped></style>
--- src/components/TranslateBox.vue
+++ src/components/TranslateBox.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+  editable: Boolean,
+
+})
+const text = defineModel()
+
+
+
+</script>
+
 <template>
-  <textarea name="" id="" maxlength="500"></textarea>
+  <div class="opacity-95 grid justify-center border-gray-700 rounded-xl border bg-gray-900  opacity-95 h-65 outline-0 w-96">
+    <textarea v-model="text" :disabled="props.editable"  @change="() => console.log(text)" class="indent-2 text-white opacity-95 h-64 outline-0 w-96 " name="" id="" maxlength="500"></textarea>
+
+  </div>
 </template>
